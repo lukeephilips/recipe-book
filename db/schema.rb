9d9850c1813bb778b3ac81fa7f5b15aea6a3473b
Chris Clifford and Luke Philips <chris@>
2016-12-14 11:20:02 -0800

    pass unit testing for all models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214180508) do
+ActiveRecord::Schema.define(version: 20161214185559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20161214180508) do
     t.string   "food_group"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "instructions", force: :cascade do |t|
+    t.integer "recipe_id"
+    t.integer "step"
+    t.string  "description"
   end
 
   create_table "lists", force: :cascade do |t|
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ActiveRecord::Base
   has_many :lists
   has_many :ingredients, through: :lists
+  has_many :instructions
 
   has_and_belongs_to_many :tags
 end
